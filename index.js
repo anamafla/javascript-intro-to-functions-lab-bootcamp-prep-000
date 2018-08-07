46facf1a1eb8ccd8d0e3b2cf65041a349203e039
Ana Mafla <anamafla@hotmail.com>
2018-08-07 20:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var lowercase = string.toLowerCase();
+  let uppercase = string.toUpperCase();
   if (string === lowercase)
     return ("I can't hear you!")
+  else if (string === uppercase)
+   return("YES INDEED!")
 }
